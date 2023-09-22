9bdd0d1c02eebaf7810014e9bee223f4aa184d04
hamzacyberpatcher <hamzaoffuture@gmail.com>
2023-09-22 17:40:34 +0500

    Added index.html

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from api import Pokemon
 from flask import Flask, render_template
+from cs50 import SQL
+
+db = SQL("sqlite:///evolutions.db")
+pokelist = db.execute("SELECT name FROM evolutions")
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return render_template("layout.html")
+	return render_template("index.html",pokelist=pokelist)
