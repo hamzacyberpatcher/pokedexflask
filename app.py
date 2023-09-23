3fcca8897f299cbb3da1bf0cd6c13f366b9ecf55
hamzacyberpatcher <hamzaoffuture@gmail.com>
2023-09-23 18:44:56 +0500

    Added random background colors

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from api import Pokemon,listpokemon,name,weight,height
 from flask import Flask, render_template, request, redirect
 from cs50 import SQL
+import random
 
 
 app = Flask(__name__)
@@ -8,17 +9,32 @@ app.jinja_env.filters["name"] = name
 app.jinja_env.filters["weight"] = weight
 app.jinja_env.filters["height"] = height
 
+bgcolor = [
+	'#F7CEE2',
+	'springgreen'
+	'lightblue',
+	'lightyellow',
+	'#CCCAF0',
+	'#CFDFEF',
+	'#FEFBBE',
+	'#9AE49E',
+	'#76CBF0',
+	'#FF6969'
+]
+
 pokelist = listpokemon()
 
 @app.route("/")
 def index():
-	return render_template("index.html",pokelist=pokelist)
+	n = random.randint(0,len(bgcolor))
+	return render_template("index.html",pokelist=pokelist,bgcolor=bgcolor[n])
 
 @app.route("/search")
 def search():
 	pokemon = request.args.get("pokemon")
 	pokedata = Pokemon(pokemon)
+	n = random.randint(0,len(bgcolor))
 	if pokedata == None:
-		return render_template("failure.html",pokemon=pokemon,pokelist=pokelist)
+		return render_template("failure.html",pokemon=pokemon,pokelist=pokelist,bgcolor=bgcolor[n])
 
-	return render_template("pokemon.html",pokedata=pokedata,pokelist=pokelist)
+	return render_template("pokemon.html",pokedata=pokedata,pokelist=pokelist,bgcolor=bgcolor[n])
